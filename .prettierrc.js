534efb1f44d2bce69fcab637df8184ec874b2bdf
reyemgs <thetortyan@gmail.com>
2020-12-09 10:31:20 +0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  overrides: [
+    {
+      files: '*.js',
+      parser: 'babel',
+    },
+    {
+      files: '*.vue',
+      parser: 'vue',
+    },
+  ],
+};
